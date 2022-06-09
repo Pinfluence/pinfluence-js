4afe998a2fd167e312e6c543816913799eba2505
Rafael Soares <rafaelsoares@fastmail.com>
2022-06-08 23:04:18 -0300

    Fix the create person test

--- data-source.ts
+++ data-source.ts
@@ -1,17 +1,38 @@
 require("reflect-metadata")
-import { DataSource } from "typeorm";
+import { DataSource, DataSourceOptions } from "typeorm";
 
 require('dotenv').config()
 
-export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: process.env.PGHOST,
-    port: 5432,
-    username: process.env.PGUSER,
-    password: process.env.PGPASSOWRD,
-    database: process.env.PGDATABASE,
-    entities: ["entity/*.ts"],
-    migrations: ["db/*.ts"],
-    synchronize: (process.env.TYPEORM_SYNC === 'true'),
-    logging: true
-})
+let DBConfig : DataSourceOptions;
+
+if (process.env.NODE_ENV === "test") {
+    DBConfig = {
+        type: "postgres",
+        host: process.env.PGHOST,
+        port: 5432,
+        username: process.env.PGUSER,
+        password: process.env.PGPASSOWRD,
+        database: "pinfluence_test",
+        entities: ["entity/*.ts"],
+        migrations: ["db/*.ts"],
+        synchronize: true,
+        migrationsRun: true,
+        dropSchema: true,
+        logging: false
+    }
+} else {
+    DBConfig = {
+        type: "postgres",
+        host: process.env.PGHOST,
+        port: 5432,
+        username: process.env.PGUSER,
+        password: process.env.PGPASSOWRD,
+        database: process.env.PGDATABASE,
+        entities: ["entity/*.ts"],
+        migrations: ["db/*.ts"],
+        synchronize: (process.env.TYPEORM_SYNC === 'true'),
+        logging: true
+    }
+}
+
+export const AppDataSource = new DataSource(DBConfig);
